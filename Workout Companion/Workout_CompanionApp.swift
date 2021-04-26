8199b2651b46a5ad0d3ec6b2de43c1217ce2abe9
Mario Eguiluz <mario@myplacesandme.com>
2021-04-26 22:59:57 +0100

    Initial Commit

---
+++ Workout Companion/Workout_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_CompanionApp.swift
+//  Workout Companion
+//
+//  Created by Mario Eguiluz on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Workout_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
